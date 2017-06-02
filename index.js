605d4cc1d8a6f79634c2cf009344475940717dd8
Santiago Gonzalez <hernangt12re3@gmail.com>
2017-06-02 12:41:51 -0500

    Making the bot to speak

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict'
 
+const token = process.env.FB_ACCESS_TOKEN
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token: token},
+	    method: 'POST',
+		json: {
+		    recipient: {id: sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -15,16 +37,25 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	// res.send('Hello world, I am a chat bot')
-	res.send('102821180318365');
+	res.send('Hello world, I am a chat bot')
 })
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
+	// if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	// 	res.send(req.query['hub.challenge'])
+	// }
+	// res.send('Error, wrong token')
+	let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
 })
 
 // Spin up the server
